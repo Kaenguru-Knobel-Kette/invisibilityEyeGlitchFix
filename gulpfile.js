6bb158f5b1e46780746859aa42b7c793caa937b3
Kaenguru-Knobel-Kette <manuel.nowack@gmail.com>
2018-12-13 22:54:53 +0100

    All js files use tabs

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,52 @@
-const del = require('del'),
-	  fs = require('fs'),
-      gulp = require('gulp'),
-      concat = require('gulp-concat'),
-      zip = require('gulp-zip'),
-      zEditPath = 'C:/Users/user/Documents/Skyrim Tools/zEdit_Alpha_v0.5.3';
+const del = require('del');
+const fs = require('fs');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const zip = require('gulp-zip');
+const zEditPath = 'C:/Users/user/Documents/Skyrim Tools/zEdit_Alpha_v0.5.3';
 
 gulp.task('clean-dist', function() {
-    return del('dist');
+	return del('dist');
 });
 
 gulp.task('build-dist', function() {
-    gulp.src(['index.js', 'src/*.js'])
-        .pipe(concat('index.js'))
-        .pipe(gulp.dest('dist'));
+	gulp.src(['index.js', 'src/*.js'])
+		.pipe(concat('index.js'))
+		.pipe(gulp.dest('dist'));
 
-    gulp.src('partials/*.html')
-        .pipe(gulp.dest('dist/partials'));
+	gulp.src('partials/*.html')
+		.pipe(gulp.dest('dist/partials'));
 
-    return gulp.src('module.json')
-        .pipe(gulp.dest('dist'));
+	return gulp.src('module.json')
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('uninstall-in-zedit', function() {
-    let moduleInfo = JSON.parse(fs.readFileSync('module.json')),
-        installationPath = `${zEditPath}/modules/${moduleInfo.id}`;
+	let moduleInfo = JSON.parse(fs.readFileSync('module.json')),
+		installationPath = `${zEditPath}/modules/${moduleInfo.id}`;
 
-    return del(installationPath, {force:true});
+	return del(installationPath, {force:true});
 });
 
 gulp.task('install-in-zedit', function() {
-    let moduleInfo = JSON.parse(fs.readFileSync('module.json')),
-        installationPath = `${zEditPath}/modules/${moduleInfo.id}`;
+	let moduleInfo = JSON.parse(fs.readFileSync('module.json')),
+		installationPath = `${zEditPath}/modules/${moduleInfo.id}`;
 
-    return gulp.src('dist/**/*')
-        .pipe(gulp.dest(installationPath));
+	return gulp.src('dist/**/*')
+		.pipe(gulp.dest(installationPath));
 });
 
 gulp.task('release', function() {
-    let moduleInfo = JSON.parse(fs.readFileSync('module.json')),
-        moduleId = moduleInfo.id,
-        moduleVersion = moduleInfo.version,
-        zipFileName = `${moduleId}-v${moduleVersion}.zip`;
+	let moduleInfo = JSON.parse(fs.readFileSync('module.json')),
+		moduleId = moduleInfo.id,
+		moduleVersion = moduleInfo.version,
+		zipFileName = `${moduleId}-v${moduleVersion}.zip`;
 
-    console.log(`Packaging ${zipFileName}`);
+	console.log(`Packaging ${zipFileName}`);
 
-    return gulp.src('dist/**/*', { base: 'dist/'})
-        .pipe(zip(zipFileName))
-        .pipe(gulp.dest('releases'));
+	return gulp.src('dist/**/*', { base: 'dist/'})
+		.pipe(zip(zipFileName))
+		.pipe(gulp.dest('releases'));
 });
 
 gulp.task('build', gulp.series('clean-dist', 'build-dist'));
